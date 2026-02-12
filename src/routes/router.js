f2fdd0d01178a5c5c04d8687d1deba68bf37d3d5
Shubham Dhankar <shubhamqapd@gmail.com>
2026-02-12 14:48:16 +0530

    git push origin mainRevert "Merge pull request #1 from shubham-4321/dev"
    This reverts commit 20fd9e2372f3e6260ddb93d2ed5b2a22bfc16dcb, reversing
    changes made to 63b9125c1685d3ee6fc948024ff58ce45ce23e2f.

--- src/routes/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const verification = require("../middleware/authJwt")
-const {dashboard, getUser} = require("../controller/controllers")
-
-router.get("/protected",verification,dashboard)
-router.get("/getuser/:id",verification,getUser)
-module.exports = router
